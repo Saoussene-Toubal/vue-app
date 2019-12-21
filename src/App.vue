aef33cb65e24909b03246f6c7eb072b8c838fe6f
suuuzana <toubalsaoussene@gmail.com>
2019-12-21 20:35:44 +0100

    vuejs app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to project of saoussene'
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: blueviolet;
+  font-weight:normal;
+  text-align:  center;
+}
+</style>
